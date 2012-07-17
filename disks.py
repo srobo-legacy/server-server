b7a15df69c83c0316ec177cbf1b79448defc451e
Rob Spanton <rspanton@zepler.net>
2012-07-17 02:16:37 +0100

    Put the disk images in a 'disks' subdirectory
    Reduce some clutter

---
+++ disks.py
@@ -0,0 +1,7 @@
+import os
+
+DISKS_DIR = os.path.join( os.path.dirname( __file__ ), "disks" )
+
+HD_BOOT = os.path.join( DISKS_DIR, "hd-boot" )
+HD_ROOT = os.path.join( DISKS_DIR, "hd-root" )
+KS_DISK = os.path.join( DISKS_DIR, "ks-disk" )
