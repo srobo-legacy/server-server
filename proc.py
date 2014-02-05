bf21656bf03ec86aea6779a2df7fc5e7e9a3f1cb
Rob Spanton <rspanton@zepler.net>
2014-02-05 00:11:12 +0000

    Use libvirt to create image
    Rather than using a load of custom stuff, just run `virt-install` to
    create the image.  Much simpler!

--- proc.py
+++
@@ -1,9 +0,0 @@
-# Simple wrapper around subprocess
-from subprocess import check_call
-
-def run( *args, **kw ):
-    "Wrapper around check_call that sets shell to True"
-    kw["shell"] = True
-
-    return check_call( *args, **kw )
-
